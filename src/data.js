74d9c10c7d84bbaee9ee27111ce1de6d63830178
Ankur Gupta <ankur.gupta@synfioo.com>
2020-08-17 12:57:49 +0200

    updated product

--- src/data.js
+++ src/data.js
@@ -95,7 +95,19 @@ export const storeProducts = [
     count: 0,
     total: 0,
   },
-];
+  {
+    id: 9,
+    title: "iPhone 11 Pro Max",
+    img: "img/product-9.png",
+    price: 1000,
+    company: "apple",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+]
 
 export const detailProduct = {
   id: 1,
@@ -108,4 +120,4 @@ export const detailProduct = {
   inCart: false,
   count: 0,
   total: 0,
-};
+}
