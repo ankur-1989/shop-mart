3bbe79da3b07bcaaff72c3d7b9828839fdcb421f
Ankur Gupta <ankur.gupta@synfioo.com>
2020-09-15 20:39:12 +0200

    added more menu items

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,7 +1,8 @@
 import React from "react"
 import * as FaIcons from "react-icons/fa"
 import * as AiIcons from "react-icons/ai"
-
+import * as GiIcons from "react-icons/gi"
+import * as FcIcons from "react-icons/fc"
 export const SidebarData = [
   {
     title: "Home",
@@ -25,6 +26,24 @@ export const SidebarData = [
     title: "Shoes",
     path: { pathname: "/products", state: "shoes" },
     icon: <FaIcons.FaShoePrints />,
+    cName: "sb-nav-text",
+  },
+  {
+    title: "Electronics",
+    path: { pathname: "/products", state: "electronics" },
+    icon: <FcIcons.FcElectronics />,
+    cName: "sb-nav-text",
+  },
+  {
+    title: "Books",
+    path: { pathname: "/products", state: "books" },
+    icon: <GiIcons.GiBookshelf />,
+    cName: "sb-nav-text",
+  },
+  {
+    title: "Sports",
+    path: { pathname: "/products", state: "sports" },
+    icon: <FcIcons.FcSportsMode />,
     cName: "sb-nav-text",
   },
 ]
